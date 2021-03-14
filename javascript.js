2dfd6c315379266078b8fe32aadb214605eec35c
nancy88199488 <nancykemuntosalome@gmail.com>
2021-03-14 14:54:22 +0300

    add the above file

--- javascript.js
+++ javascript.js
@@ -6,7 +6,10 @@ function triangle() {
     var [a,b,c] = sides;
     var display = document.getElementById("display");
     var explanation = document.getElementById("explanation");
-    
+    if(a. length === 0 || b.length === 0 || c.length === 0 ) {
+        
+    }
+
 
     
 }
