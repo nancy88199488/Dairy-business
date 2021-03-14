0b90b367e9a6a40a580091d7dc89d2a5cb1156b6
nancy88199488 <nancykemuntosalome@gmail.com>
2021-03-14 14:33:13 +0300

    add the above file

--- javascript.js
+++ javascript.js
@@ -2,5 +2,6 @@ function triangle() {
     var sides = [];
     sides.push(ducument.getElementById("num1").value);
     sides.push(document.getElementById("num2").value);
+    sides.push(document.getElementById("num3").value);
     
 }
