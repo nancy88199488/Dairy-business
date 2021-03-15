25cfe553feb43d40d600f1b09cb6a2115d9a1429
nancy88199488 <nancykemuntosalome@gmail.com>
2021-03-15 06:11:22 +0300

    add the above file

--- javascript.js
+++ javascript.js
@@ -3,20 +3,22 @@ function triangle() {
     sides.push(ducument.getElementById("num1").value);
     sides.push(document.getElementById("num2").value);
     sides.push(document.getElementById("num3").value);
-    var [a,b,c] = sides;
+    
+    var [a,b,c,] = sides;
     var display = document.getElementById("display");
     var explanation = document.getElementById("explanation");
-    if(a. length === 0 || b.length === 0 || c.length === 0 ) {
+    if(a. length === 0 || b.length === 0 || c.length === 0) {
         display.innerHTML = "<p>please <span class='fill>FILL</span> all the fields </p>"
 
     } else {
         a = parseFloat(a);
         b = parseFloat(b);
         c = parseFloat(c);
-        track(a,b,c);
+        
+        track(a,b,c,);
     }
 }
-function track(a,b,c) {
+function track(a,b,c,) {
     if(a + b > c && b + c > a && c + a > b) {
         if(a === b && a === c) {
             display.innerHTML = "< class='display-text'>That is an <span class='type'>Equilateral</span> triangle<p><span class='fill'>ALL</span> sides are equal";
